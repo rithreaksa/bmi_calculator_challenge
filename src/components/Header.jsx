b1a5604a2b31506d691746151626e1e68d253b43
Rithreaksa <wonderfulreaksa@gmail.com>
2020-11-17 13:31:24 +0100

    Adds color to BMI calculator text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = (props) => {
   return (
-    <h1 style={{ textAlign: "center" }} {...props}>
+    <h1 style={{ textAlign: "center", color: "teal"}} {...props}>
       {props.children}
     </h1>
   );
